929b849d6d669d29b805871ef5f18516e4b901c9
Ian Williamson <ianwill2014@gmail.com>
2025-01-20 15:14:52 -0600

    finished routing

--- main.go
+++ main.go
@@ -1,12 +1,57 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"strconv"
+	"sync/atomic"
 )
 
 func main() {
+	const port = "8080"
 	mux := http.NewServeMux()
-	server := http.Server{Handler: mux, Addr: ":8080"}
+	srv := &http.Server{
+		Handler: mux,
+		Addr:    ":" + port,
+	}
 
-	server.ListenAndServe()
+	apiCfg := &apiConfig{}
+
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir("./app")))))
+
+	mux.HandleFunc("GET /healthz", healthResp)
+	mux.HandleFunc("GET /metrics", apiCfg.countResp)
+	mux.HandleFunc("POST /reset", apiCfg.countReset)
+
+	log.Printf("Serving on port: %s\n", port)
+	log.Fatal(srv.ListenAndServe())
+}
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) countResp(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hits: " + strconv.Itoa(int(cfg.fileserverHits.Load()))))
+}
+
+func (cfg *apiConfig) countReset(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	_ = cfg.fileserverHits.Swap(0)
+}
+
+func healthResp(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
