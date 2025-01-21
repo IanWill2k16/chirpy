124ba754814a312dc9809f86c0efe4cd5ae92d84
Ian Williamson <ianwill2014@gmail.com>
2025-01-21 13:21:07 -0600

    finished storage

--- handlers.go
+++ handlers.go
@@ -6,14 +6,21 @@ import (
 	"net/http"
 	"strings"
 	"sync/atomic"
+	"time"
+
+	"github.com/IanWill2k16/chirpy/internal/database"
+	"github.com/google/uuid"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	dbQueries      *database.Queries
+	envPlatform    string
 }
 
 type parameters struct {
-	Body string `json:"body"`
+	Body   string    `json:"body"`
+	UserID uuid.UUID `json:"user_id"`
 }
 
 type cleanedParameters struct {
@@ -22,6 +29,21 @@ type cleanedParameters struct {
 
 type validParameters struct {
 	Valid bool `json:"valid"`
+}
+
+type User struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Email     string    `json:"email"`
+}
+
+type Chirp struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at`
+	Body      string    `json:"body"`
+	UserID    uuid.UUID `json:"user_id"`
 }
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
@@ -44,8 +66,15 @@ func (cfg *apiConfig) countResp(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(fmt.Sprintf(htmlTemplate, int(cfg.fileserverHits.Load()))))
 }
 
-func (cfg *apiConfig) countReset(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+func (cfg *apiConfig) adminReset(w http.ResponseWriter, r *http.Request) {
+	if cfg.envPlatform != "dev" {
+		returnError(w, "Forbidden", 403)
+		return
+	}
+	err := cfg.dbQueries.DeleteAllUsers(r.Context())
+	if err != nil {
+		returnError(w, fmt.Sprintf("Failed to delete users: %v", err), 500)
+	}
 	w.WriteHeader(http.StatusOK)
 	_ = cfg.fileserverHits.Swap(0)
 }
@@ -56,7 +85,7 @@ func healthResp(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("OK"))
 }
 
-func validateChirpHandler(w http.ResponseWriter, r *http.Request) {
+func (cfg *apiConfig) createChirp(w http.ResponseWriter, r *http.Request) {
 	params := parameters{}
 	err := decodeJSON(r, &params)
 	if err != nil {
@@ -83,9 +112,99 @@ func validateChirpHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	cleanParams.CleanBody = strings.Join(words, " ")
 
-	err = encodeJSON(w, &cleanParams, 200)
+	chirpParams := database.CreateChirpParams{
+		Body:   cleanParams.CleanBody,
+		UserID: params.UserID,
+	}
+
+	resp, err := cfg.dbQueries.CreateChirp(r.Context(), chirpParams)
+	if err != nil {
+		returnError(w, "Something went wrong", 500)
+		return
+	}
+
+	chirpReturn := Chirp{
+		ID:        resp.ID,
+		CreatedAt: resp.CreatedAt,
+		UpdatedAt: resp.UpdatedAt,
+		Body:      resp.Body,
+		UserID:    resp.UserID,
+	}
+
+	err = encodeJSON(w, &chirpReturn, 201)
 	if err != nil {
 		log.Printf("error encoding JSON: %v", err)
 		returnError(w, "Something went wrong", 500)
 	}
+}
+
+func (cfg *apiConfig) createUser(w http.ResponseWriter, r *http.Request) {
+	type expectedInput struct {
+		Email string `json:"email"`
+	}
+	requestInput := expectedInput{}
+	err := decodeJSON(r, &requestInput)
+	if err != nil {
+		returnError(w, "Something went wrong", 400)
+		return
+	}
+	userData, err := cfg.dbQueries.CreateUser(r.Context(), requestInput.Email)
+	if err != nil {
+		log.Printf("error creating user: %v", err)
+		returnError(w, "Error creating user", 500)
+		return
+	}
+	userReturn := User{
+		ID:        userData.ID,
+		CreatedAt: userData.CreatedAt,
+		UpdatedAt: userData.UpdatedAt,
+		Email:     userData.Email,
+	}
+	err = encodeJSON(w, userReturn, 201)
+	if err != nil {
+		returnError(w, "Something went wrong", 500)
+	}
+}
+
+func (cfg *apiConfig) getChirps(w http.ResponseWriter, r *http.Request) {
+	allChirps, err := cfg.dbQueries.GetAllChirps(r.Context())
+	if err != nil {
+		returnError(w, "Something went wrong", 500)
+		return
+	}
+	allChirpsReturn := []Chirp{}
+	for _, chirp := range allChirps {
+		formattedChirp := Chirp{
+			ID:        chirp.ID,
+			CreatedAt: chirp.CreatedAt,
+			UpdatedAt: chirp.UpdatedAt,
+			Body:      chirp.Body,
+			UserID:    chirp.UserID,
+		}
+		allChirpsReturn = append(allChirpsReturn, formattedChirp)
+	}
+
+	err = encodeJSON(w, allChirpsReturn, 200)
+	if err != nil {
+		returnError(w, "Something went wrong", 500)
+	}
+}
+
+func (cfg *apiConfig) getSingleChirp(w http.ResponseWriter, r *http.Request) {
+	chirpID, err := uuid.Parse(r.PathValue("chirpID"))
+	if err != nil {
+		returnError(w, fmt.Sprintf("Something went wrong: %v", chirpID), 500)
+	}
+	res, err := cfg.dbQueries.GetOneChirp(r.Context(), chirpID)
+	if err != nil {
+		returnError(w, "Chirp not found", 404)
+	}
+	chirpResponse := Chirp{
+		ID:        res.ID,
+		CreatedAt: res.CreatedAt,
+		UpdatedAt: res.UpdatedAt,
+		Body:      res.Body,
+		UserID:    res.UserID,
+	}
+	encodeJSON(w, chirpResponse, 200)
 }
--- main.go
+++ main.go
@@ -1,11 +1,32 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/IanWill2k16/chirpy/internal/database"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 func main() {
+	godotenv.Load()
+
+	dbURL := os.Getenv("DB_URL")
+	envPlatformVar := os.Getenv("PLATFORM")
+
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Printf("error connect to database: %v", err)
+	}
+
+	apiCfg := &apiConfig{
+		dbQueries:   database.New(db),
+		envPlatform: envPlatformVar,
+	}
+
 	const port = "8080"
 	mux := http.NewServeMux()
 	srv := &http.Server{
@@ -13,14 +34,15 @@ func main() {
 		Addr:    ":" + port,
 	}
 
-	apiCfg := &apiConfig{}
-
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir("./app")))))
 
 	mux.HandleFunc("GET /api/healthz", healthResp)
-	mux.HandleFunc("POST /api/validate_chirp", validateChirpHandler)
+	mux.HandleFunc("POST /api/users", apiCfg.createUser)
+	mux.HandleFunc("POST /api/chirps", apiCfg.createChirp)
+	mux.HandleFunc("GET /api/chirps", apiCfg.getChirps)
+	mux.HandleFunc("GET /api/chirps/{chirpID}", apiCfg.getSingleChirp)
 	mux.HandleFunc("GET /admin/metrics", apiCfg.countResp)
-	mux.HandleFunc("POST /admin/reset", apiCfg.countReset)
+	mux.HandleFunc("POST /admin/reset", apiCfg.adminReset)
 
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(srv.ListenAndServe())
